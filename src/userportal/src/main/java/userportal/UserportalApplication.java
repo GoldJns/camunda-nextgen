c3aa0bd15748a922d6ef4d416dcabd9fbf17cb06
Jonas Goldnik <jonas.goldnik@abatplus.de>
2024-09-16 10:12:19 +0200

    feat: add services basestructure with dockerfiles

---
+++ src/userportal/src/main/java/userportal/UserportalApplication.java
@@ -0,0 +1,13 @@
+package userportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserportalApplication.class, args);
+	}
+
+}
