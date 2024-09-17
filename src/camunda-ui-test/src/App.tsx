46621aec043e825ce1d2e55258353c4d673beabf
Jonas Goldnik <jonas.goldnik@abatplus.de>
2024-09-17 13:04:09 +0200

    chore: language toggle showcase

--- src/camunda-ui-test/src/App.tsx
+++ src/camunda-ui-test/src/App.tsx
@@ -2,12 +2,31 @@ import { I18nextProvider } from "react-i18next";
 import "./App.css";
 import Login from "./components/Login";
 import i18n from "./i18n/i18n";
+import { useState } from "react";
 
 function App() {
+  const [language, setLanguage] = useState("en");
+
+  const toggleLanguage = () => {
+    const newLang = language === "en" ? "de" : "en";
+    setLanguage(newLang);
+    i18n.changeLanguage(newLang);
+  };
+
   return (
     <>
       <I18nextProvider i18n={i18n}>
-        <Login />
+        <>
+          <button
+            //style={{ backgroundColor: "white", color: "black" }}
+            onClick={() => {
+              toggleLanguage();
+            }}
+          >
+            Toggle language
+          </button>
+          <Login />
+        </>
       </I18nextProvider>
     </>
   );
