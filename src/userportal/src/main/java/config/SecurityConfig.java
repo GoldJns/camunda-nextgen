618fb346441561d27b4af8620de325ebc529d22a
Zohaib Khalid <kzohaib77@gmail.com>
2024-09-21 19:55:30 +0500

    refactor url

--- src/userportal/src/main/java/config/SecurityConfig.java
+++ src/userportal/src/main/java/config/SecurityConfig.java
@@ -74,7 +74,7 @@ public class SecurityConfig {
 	public CorsConfigurationSource corsConfigurationSource() {
 		final CorsConfiguration config = new CorsConfiguration();
 
-		config.setAllowedOrigins(Arrays.asList("http://localhost:8080","http://127.0.0.1:5500", "http://localhost:5500"));
+		config.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:5173"));
 		config.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
 		config.setAllowCredentials(true);
 		config.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type", "token"));
