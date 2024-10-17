1c172b79156d6a169bb0dcb792d6a77645fe4507
wong <example@test.com>
2024-10-17 12:31:01 +0200

    chore: adjust camunda values

--- src/health-management/src/main/java/com/example/health_management/HealthManagementApplication.java
+++ src/health-management/src/main/java/com/example/health_management/HealthManagementApplication.java
@@ -2,12 +2,25 @@ package com.example.health_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.camunda.zeebe.spring.client.annotation.Deployment;
 
 @SpringBootApplication
+@Deployment(resources = "classpath:demoProcess.bpmn")
 public class HealthManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HealthManagementApplication.class, args);
 	}
+ @RestController
+    public static class HelloController {
+
+        @GetMapping("/hello")
+        public String sayHello() {
+            return "Hello, World!";
+        }
+    }
 
 }
