8c7fc51a56345501d720d0d59a17c725c568e449
Jonas Goldnik <example@test.com>
2024-10-21 17:55:02 +0200

    Revert "chore: disable camunda stuff"
    This reverts commit ca17880b0a1d409cc36f99cb01f4b6d5fec55d8a.

--- src/health-management/src/main/java/com/example/health_management/HealthManagementApplication.java
+++ src/health-management/src/main/java/com/example/health_management/HealthManagementApplication.java
@@ -6,14 +6,9 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.camunda.zeebe.client.api.response.ActivatedJob;
-import io.camunda.zeebe.spring.client.annotation.JobWorker;
-
-
-
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-public class HealthManagementApplication{
+public class HealthManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HealthManagementApplication.class, args);
@@ -25,8 +20,4 @@ public class HealthManagementApplication{
             return "Hello, World!";
         }
     }
-    @JobWorker(type = "foo")
-public void handleJobFoo(final ActivatedJob job) {
-  
-}
 }
