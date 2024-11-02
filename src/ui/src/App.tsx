1f023a41a8517e015ce95ba0c65317f2a1886cec
amza94 <115936712+amza94@users.noreply.github.com>
2024-11-02 19:35:16 +0100

    Ubdate Style Patient UI

--- src/ui/src/App.tsx
+++ src/ui/src/App.tsx
@@ -1,53 +1,26 @@
-import { I18nextProvider } from "react-i18next";
 import Login from "./components/Login";
-import i18n from "./i18n/i18n";
-import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Pdashboard from "./components/Pateetndashboard/Pdashboard";
 import Terminbuchung from "./components/Pateetndashboard/terminbuchung";
 import Rezeptanforderungen from "./components/Pateetndashboard/rezeptanforderungen";
 import "./App.css";
 
-
-
-
 function App() {
-  const [language, setLanguage] = useState("en");
-
-
-  const toggleLanguage = () => {
-    const newLang = language === "en" ? "de" : "en";
-    setLanguage(newLang);
-    i18n.changeLanguage(newLang);
-  };
-
 
   return (
     <>
-      <I18nextProvider i18n={i18n}>
       <Router>
-      <header className="basicheader">
-      <button
-              style={{ color: "black", display: "flex", alignItems: "center" }}
-              onClick={toggleLanguage}
-            >
-  <img src={language === "en" ? "/de.jfif" : "/en.png"} alt={language} style={{ width: "20px"  }} />
-            </button>
-      
-          </header>
-
-
+  
           <Routes>
                 <Route path="/" element={<Login />} />
-                <Route path="/gesundheitsdaten" element={<Pdashboard />} />
-                <Route path="/terminbuchung" element={<Terminbuchung />} />
-                <Route path="/rezeptanforderungen" element={<Rezeptanforderungen />} />
+                <Route path="/Patienten/gesundheitsdaten" element={<Pdashboard />} />
+                <Route path="/Patienten/terminbuchung" element={<Terminbuchung />} />
+                <Route path="/Patienten/rezeptanforderungen" element={<Rezeptanforderungen />} />
 
             </Routes>
      
         </Router>
 
-      </I18nextProvider>
     </>
   );
 }
--- src/ui/src/components/Pateetndashboard/Navbar.tsx
+++ src/ui/src/components/Pateetndashboard/Navbar.tsx
@@ -1,45 +1,47 @@
-import React from 'react';
+import React from "react";
 import "./navbar.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   return (
     <nav className="navbar">
+      <div className="icon-container">
+      <h2><i className="fas fa-user-injured patient-icon"></i></h2>
+
+        
+        <h2>Patient Dashboard</h2>
+      </div>
       <ul>
         <li className="var_nav">
-          <div className="link_bg"></div>
           <div className="link_title">
             <div className="icon">
               <i className="icon-home icon-2x"></i>
             </div>
-            <Link to="/gesundheitsdaten">
+            <Link to="/Patienten/gesundheitsdaten">
               <span>Gesundheitsdaten</span>
             </Link>
           </div>
         </li>
         <li className="var_nav">
-          <div className="link_bg"></div>
           <div className="link_title">
             <div className="icon">
               <i className="icon-calendar icon-2x"></i>
             </div>
-            <Link to="/terminbuchung">
+            <Link to="/Patienten/terminbuchung">
               <span>Terminbuchung</span>
             </Link>
           </div>
         </li>
         <li className="var_nav">
-          <div className="link_bg"></div>
           <div className="link_title">
             <div className="icon">
               <i className="icon-file icon-2x"></i>
             </div>
-            <Link to="/rezeptanforderungen">
+            <Link to="/Patienten/rezeptanforderungen">
               <span>Rezeptanforderungen</span>
             </Link>
           </div>
         </li>
-  
       </ul>
     </nav>
   );
