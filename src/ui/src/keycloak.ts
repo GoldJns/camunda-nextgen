3941873555d7b2dbd356e066cd8dd88255cd150f
Zohaib Khalid <kzohaib77@gmail.com>
2024-11-04 14:43:56 +0100

    add dependency / change client ID

--- src/ui/src/keycloak.ts
+++ src/ui/src/keycloak.ts
@@ -2,5 +2,5 @@ import Keycloak from "keycloak-js";
 export const keycloak = new Keycloak({
   url: "http://localhost:18080/auth",
   realm: "camunda-platform",
-  clientId: "b1f8e2ba-4002-4b07-9106-1871a90e439f",
+  clientId: "536d1f48-1b3b-4717-9ed2-069beaa78aa1",
 });
