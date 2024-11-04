017e435e9364197d5b1f40ecda9207b4839e4687
Jonas Goldnik <jonas.goldnik@abatplus.de>
2024-11-04 13:33:37 +0100

    fix: keycloak config

--- src/ui/src/keycloak.ts
+++ src/ui/src/keycloak.ts
@@ -1,6 +1,6 @@
 import Keycloak from "keycloak-js";
 export const keycloak = new Keycloak({
-  url: "http://localhost:8080/auth",
-  realm: "Keycloak-react-auth",
-  clientId: "React-auth",
+  url: "http://localhost:18080/auth",
+  realm: "camunda-platform",
+  clientId: "b1f8e2ba-4002-4b07-9106-1871a90e439f",
 });
