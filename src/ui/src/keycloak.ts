f7733489098ce8924248cd9146ae94da729e06ab
Zohaib Khalid <kzohaib77@gmail.com>
2024-11-05 20:40:56 +0100

    update pom / client id in ui

--- src/ui/src/keycloak.ts
+++ src/ui/src/keycloak.ts
@@ -2,5 +2,5 @@ import Keycloak from "keycloak-js";
 export const keycloak = new Keycloak({
   url: "http://localhost:18080/auth",
   realm: "camunda-platform",
-  clientId: "536d1f48-1b3b-4717-9ed2-069beaa78aa1",
+  clientId: "7513f28f-4e0f-46ce-8251-1c62ad96cc751",
 });
