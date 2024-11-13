2f881772d0a38a7984ff7b06d8829d1b89b16d74
Zohaib Khalid <kzohaib77@gmail.com>
2024-11-13 21:24:42 +0100

    add application in app package

--- src/health-management/src/main/java/com/example/health_management/app/HealthManagementApplication.java
+++ src/health-management/src/main/java/com/example/health_management/app/HealthManagementApplication.java
@@ -1,13 +1,14 @@
-package com.example.health_management;
+package com.example.health_management.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import io.camunda.zeebe.spring.client.annotation.Deployment;
 
 @SpringBootApplication
 @Deployment(resources = "classpath*:/bpmn/**/")
-
+@ComponentScan(basePackages={"appointment", "healthrecord", "prescription", "task", "user"})
 public class HealthManagementApplication {
 
 	public static void main(String[] args) {
