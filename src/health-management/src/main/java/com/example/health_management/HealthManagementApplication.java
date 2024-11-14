26d71adef3a3e75acaa49917daadd6e1458f76cf
Zohaib Khalid <kzohaib77@gmail.com>
2024-11-14 12:58:51 +0100

    feature resetet

--- src/health-management/src/main/java/com/example/health_management/HealthManagementApplication.java
+++ src/health-management/src/main/java/com/example/health_management/HealthManagementApplication.java
@@ -1,14 +1,13 @@
-package com.example.health_management.app;
+package com.example.health_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 import io.camunda.zeebe.spring.client.annotation.Deployment;
 
 @SpringBootApplication
 @Deployment(resources = "classpath*:/bpmn/**/")
-@ComponentScan(basePackages={"appointment", "healthrecord", "prescription", "task", "user"})
+
 public class HealthManagementApplication {
 
 	public static void main(String[] args) {
--- src/health-management/src/main/resources/db/migration/V1__init.sql
+++ src/health-management/src/main/resources/db/migration/V1__init.sql
@@ -1,28 +1,20 @@
+
+
 CREATE TABLE health_records (
-                            id SERIAL PRIMARY KEY,
-                            username VARCHAR(10) NOT NULL UNIQUE,
-                            allergies VARCHAR(255),
-                            chronic_conditions VARCHAR(255),
-                            surgeries VARCHAR(255),
-                            health_insurance VARCHAR(255),
-                            FOREIGN KEY (username) REFERENCES public.user_entity(username) ON DELETE CASCADE
-                            );
+                                id SERIAL PRIMARY KEY,
+                                username VARCHAR(10) NOT NULL UNIQUE,
+                                allergies VARCHAR(255),
+                                chronic_conditions VARCHAR(255),
+                                surgeries VARCHAR(255),
+                                health_insurance VARCHAR(255),
+                                FOREIGN KEY (username) REFERENCES public.user_entity(username) ON DELETE CASCADE
+);
 
 CREATE TABLE visits (
-                    id SERIAL PRIMARY KEY,
-                    health_record_id BIGINT,
-                    date_of_visit DATE NOT NULL,
-                    diagnosis VARCHAR(255),
-                    treatment VARCHAR(255),
-                    FOREIGN KEY (health_record_id) REFERENCES health_records(id) ON DELETE CASCADE
-                    );
-
-CREATE TABLE appointment (
                         id SERIAL PRIMARY KEY,
-                        userID varchar(36),
-                        month varchar(255),
-                        day varchar(255),
-                        date DATE,
-                        time TIME,
-                        FOREIGN KEY (userID) REFERENCES public.user_entity(id) ON DELETE CASCADE
-                        );
+                        health_record_id BIGINT,
+                        date_of_visit DATE NOT NULL,
+                        diagnosis VARCHAR(255),
+                        treatment VARCHAR(255),
+                        FOREIGN KEY (health_record_id) REFERENCES health_records(id) ON DELETE CASCADE
+);
