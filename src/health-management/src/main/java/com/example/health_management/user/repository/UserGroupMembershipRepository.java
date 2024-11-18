a3c35cac33cfed18a5e3775f4d8c1a6ba0a63d83
Zohaib Khalid <kzohaib77@gmail.com>
2024-11-18 08:39:58 +0100

    refactor primary key

--- src/health-management/src/main/java/com/example/health_management/user/repository/UserGroupMembershipRepository.java
+++ src/health-management/src/main/java/com/example/health_management/user/repository/UserGroupMembershipRepository.java
@@ -3,10 +3,11 @@ package com.example.health_management.user.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.health_management.user.model.UserGroupMembershipEntity;
+import com.example.health_management.user.model.UserGroupMembershipId;
 
 import java.util.List;
 
-public interface UserGroupMembershipRepository extends JpaRepository<UserGroupMembershipEntity, String> {
+public interface UserGroupMembershipRepository extends JpaRepository<UserGroupMembershipEntity, UserGroupMembershipId> {
 
     List<UserGroupMembershipEntity> findByGroupId(String groupId);
     
