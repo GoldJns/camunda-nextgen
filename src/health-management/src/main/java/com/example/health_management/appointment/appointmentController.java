ad828fb2e56f29a5ce29b40fbe9d01533844d329
Zohaib Khalid <kzohaib77@gmail.com>
2024-11-18 14:35:06 +0100

    refactor name to Pascalcase

--- src/health-management/src/main/java/com/example/health_management/appointment/appointmentController.java
+++ src/health-management/src/main/java/com/example/health_management/appointment/appointmentController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/appoint")
-public class appointmentController {
+public class AppointmentController {
     
     @Autowired
     private AppointmentService appointmentService;
