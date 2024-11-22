e2e108bac6460f6a85b5bc402f55bd21b618e9c9
Zohaib Khalid <kzohaib77@gmail.com>
2024-11-22 16:47:37 +0100

    rmv dummy data/ add actual data

--- src/ui/src/components/DoctorDashboard/AppointmentManagerContainer.tsx
+++ src/ui/src/components/DoctorDashboard/AppointmentManagerContainer.tsx
@@ -4,14 +4,13 @@ import "./css/Doctor.css";
 interface Appointment {
   id: number;
   userId: string;
-  patientname: string;
+  patientName: string;
   docName: string;
   date: string;
   time: string;
 }
 
 const accessToken = sessionStorage.getItem("accessToken")!;
-// const userId = sessionStorage.getItem("userId");
 const username = sessionStorage.getItem("username")!;
 const APPOINTMENT_URL = "http://localhost:8080/api/appoint";
 
@@ -53,43 +52,15 @@ const AppointmentManagerContainer: React.FC = () => {
     setDoctorAppointments(appointments);
   };
 
+  // ====================== Helper Methods =======================
+  const formatDateToGerman = (date: Date) => {
+    return date.toLocaleDateString("de-DE");
+  };
+
   useEffect(() => {
     loadAppointments();
   }, []);
 
-  const appointments = [
-      { patientName: "AMIRA YER", date: "2024-10-01", time: "10:00" },
-      { patientName: "Jane D", date: "2024-10-02", time: "11:00" },
-      { patientName: "SOSO J", date: "2024-10-03", time: "1:00" },
-    ];
-
-      // const [waitingList, setWaitingList] = React.useState([
-      //   { patientName: "SS Brown", date: "2024-10-01", time: "10:00", status: "Pending" },
-      //   { patientName: "Charlie Black", date: "2024-10-01", time: "10:00", status: "Pending" },
-      //   { patientName: "Diana Green", date: "2024-10-01", time: "10:00", status: "Pending" },
-      // ]);
-    
-      // Function to handle acceptance of a patient
-      // const handleAccept = (index: number) => {
-      //   const updatedList = waitingList.map((waiting, i) => {
-      //     if (i === index) {
-      //       return { ...waiting, status: "Accepted" };
-      //     }
-      //     return waiting;
-      //   });
-      //   setWaitingList(updatedList);
-      // };
-    
-      // Function to handle rejection of a patient
-      // const handleReject = (index: number) => {
-      //   const updatedList = waitingList.map((waiting, i) => {
-      //     if (i === index) {
-      //       return { ...waiting, status: "Rejected" };
-      //     }
-      //     return waiting;
-      //   });
-      //   setWaitingList(updatedList);
-      // };   
   return (
     <div className="docdiv">
       <div className="Titles">
@@ -107,48 +78,16 @@ const AppointmentManagerContainer: React.FC = () => {
               </tr>
             </thead>
             <tbody>
-              {appointments.map((appointment, index) => (
+              {doctorAppointments.map((appointment, index) => (
                 <tr key={index}>
                   <td>{appointment.patientName}</td>
-                  <td>{appointment.date}</td>
-                  <td>{appointment.time}</td>
+                  <td>{formatDateToGerman(new Date(appointment.date))}</td>
+                  <td>{appointment.time.slice(0,5)}</td>
                 </tr>
               ))}
             </tbody>
           </table>
         </div>
-        {/* <div className="waitings">
-        <h2>Warteliste</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Patientenname</th>
-                <th>Datum</th>
-                <th>Zeit</th>
-                <th>Status</th>
-                <th>Aktionen</th>
-              </tr>
-            </thead>
-            <tbody>
-              {waitingList.map((waiting, index) => (
-                <tr key={index}>
-                  <td>{waiting.patientName}</td>
-                  <td>{waiting.date}</td>
-                  <td>{waiting.time}</td>
-                  <td>{waiting.status}</td>
-                  <td>
-                  <button onClick={() => handleAccept(index)} style={{ background: 'none', border: 'none', cursor: 'pointer' }}>
-                      <i className="fas fa-check-circle" style={{ color: 'green', fontSize: '20px' }}></i>
-                    </button>
-                    <button onClick={() => handleReject(index)} style={{ background: 'none', border: 'none', marginLeft: '5px' , cursor: 'pointer' }}>
-                      <i className="fas fa-times-circle" style={{ color: 'red', fontSize: '20px' }}></i>
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div> */}
       </div>
     </div>
   );
