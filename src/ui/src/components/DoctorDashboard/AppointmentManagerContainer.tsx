fbc29170b6ba32cb85281f896f9f83e648f4b617
Zohaib Khalid <kzohaib77@gmail.com>
2024-11-22 16:24:35 +0100

    add initial changes

--- src/ui/src/components/DoctorDashboard/AppointmentManagerContainer.tsx
+++ src/ui/src/components/DoctorDashboard/AppointmentManagerContainer.tsx
@@ -1,39 +1,95 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./css/Doctor.css";
 
+interface Appointment {
+  id: number;
+  userId: string;
+  patientname: string;
+  docName: string;
+  date: string;
+  time: string;
+}
+
+const accessToken = sessionStorage.getItem("accessToken")!;
+// const userId = sessionStorage.getItem("userId");
+const username = sessionStorage.getItem("username")!;
+const APPOINTMENT_URL = "http://localhost:8080/api/appoint";
+
 const AppointmentManagerContainer: React.FC = () => {
-    const appointments = [
-        { patientName: "AMIRA YER", date: "2024-10-01", time: "10:00" },
-        { patientName: "Jane D", date: "2024-10-02", time: "11:00" },
-        { patientName: "SOSO J", date: "2024-10-03", time: "1:00" },
-      ];
-      const [waitingList, setWaitingList] = React.useState([
-        { patientName: "SS Brown", date: "2024-10-01", time: "10:00", status: "Pending" },
-        { patientName: "Charlie Black", date: "2024-10-01", time: "10:00", status: "Pending" },
-        { patientName: "Diana Green", date: "2024-10-01", time: "10:00", status: "Pending" },
-      ]);
+
+  const [doctorAppointments, setDoctorAppointments] = useState<Appointment[]>([]);
+
+  // ====================== Read Appointment By Username =======================
+  const fetchAppointmentsByUsername = async (): Promise<Appointment[]> => {
+    try {
+        const response = await fetch(APPOINTMENT_URL+"/byusername/"+username, {
+          method: 'GET',
+          headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          return data || [];
+        } else {
+          console.error('Failed to fetch appointments by username:', response.status);
+          return [];
+        }
+    } catch (error) {
+        console.error('Error fetching appointments by username:', error);
+        return [];
+    }
+  };
+
+  const loadAppointments = async () => {
+    const appointments = await fetchAppointmentsByUsername();
+    appointments.sort((a, b) => {
+      const dateA = new Date(a.date + 'T' + a.time);
+      const dateB = new Date(b.date + 'T' + b.time);
+      return dateA.getTime() - dateB.getTime();
+    });
+    setDoctorAppointments(appointments);
+  };
+
+  useEffect(() => {
+    loadAppointments();
+  }, []);
+
+  const appointments = [
+      { patientName: "AMIRA YER", date: "2024-10-01", time: "10:00" },
+      { patientName: "Jane D", date: "2024-10-02", time: "11:00" },
+      { patientName: "SOSO J", date: "2024-10-03", time: "1:00" },
+    ];
+
+      // const [waitingList, setWaitingList] = React.useState([
+      //   { patientName: "SS Brown", date: "2024-10-01", time: "10:00", status: "Pending" },
+      //   { patientName: "Charlie Black", date: "2024-10-01", time: "10:00", status: "Pending" },
+      //   { patientName: "Diana Green", date: "2024-10-01", time: "10:00", status: "Pending" },
+      // ]);
     
       // Function to handle acceptance of a patient
-      const handleAccept = (index: number) => {
-        const updatedList = waitingList.map((waiting, i) => {
-          if (i === index) {
-            return { ...waiting, status: "Accepted" };
-          }
-          return waiting;
-        });
-        setWaitingList(updatedList);
-      };
+      // const handleAccept = (index: number) => {
+      //   const updatedList = waitingList.map((waiting, i) => {
+      //     if (i === index) {
+      //       return { ...waiting, status: "Accepted" };
+      //     }
+      //     return waiting;
+      //   });
+      //   setWaitingList(updatedList);
+      // };
     
       // Function to handle rejection of a patient
-      const handleReject = (index: number) => {
-        const updatedList = waitingList.map((waiting, i) => {
-          if (i === index) {
-            return { ...waiting, status: "Rejected" };
-          }
-          return waiting;
-        });
-        setWaitingList(updatedList);
-      };   
+      // const handleReject = (index: number) => {
+      //   const updatedList = waitingList.map((waiting, i) => {
+      //     if (i === index) {
+      //       return { ...waiting, status: "Rejected" };
+      //     }
+      //     return waiting;
+      //   });
+      //   setWaitingList(updatedList);
+      // };   
   return (
     <div className="docdiv">
       <div className="Titles">
@@ -61,7 +117,7 @@ const AppointmentManagerContainer: React.FC = () => {
             </tbody>
           </table>
         </div>
-        <div className="waitings">
+        {/* <div className="waitings">
         <h2>Warteliste</h2>
           <table>
             <thead>
@@ -92,7 +148,7 @@ const AppointmentManagerContainer: React.FC = () => {
               ))}
             </tbody>
           </table>
-        </div>
+        </div> */}
       </div>
     </div>
   );
